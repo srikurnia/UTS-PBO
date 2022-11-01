6183b1ea8b3df980e83190759362ca55a96867e4
srikurnia <117138819+srikurnia@users.noreply.github.com>
2022-11-01 11:15:18 +0700

    Latihan PBO Awal

---
+++ LatihanPBO/src/PackageLatihanPBO/Biodata.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PackageLatihanPBO;
+
+/**
+ *
+ * @author Sri Kurnia
+ */
+public class Biodata {
+    public static void main(String[] args){
+        System.out.println("Nama : Sri Kurnia");
+        System.out.println("NIM :2001042");
+        System.out.println("Alamat : Ilung Tengah");
+    }
+}
